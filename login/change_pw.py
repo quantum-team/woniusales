52dcdb91e08a4288a0f6be2dab6ac06cc1a22380
kicebeauty <huangjun_cd@foxmail.com>
2020-07-06 19:14:43 +0800

    2020.07.06 19:14

--- login/change_pw.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '..'))
-from Common.utils import pwd_input
-from Common.utils import Mysql
-from Common.utils import encrypt
-
--- login/main.py
+++ login/main.py
@@ -1,18 +1,11 @@
 
 from login import login
-from register import register
 
 def main():
     print('{:*^60}'.format(' 欢迎来到WoniuSales！'))
-    print('{:*^54}'.format(' 1：登录   2：注册   3：修改密码 '))
-    choice = input('请选择：')
-    if choice == '1':
-        login()
-    elif choice == '2':
-        register()
-    elif choice == '3':
-        change_pw()
-    else:
-        print('输入的选项不正确，请重新输入！')
+    login_check = login()
 
-main()
+
+if __name__ == "__main__":
+    main()
+
